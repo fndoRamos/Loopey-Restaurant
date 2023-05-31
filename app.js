a157df00bdf9908043222bcbfb6ef02350c72b38
Fernando Ramos <fndo.ramos@gmail.com>
2023-05-31 14:24:30 +0200

    handlebars views layout partials

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 const express = require("express");
+const hbs = require("hbs")
 
 const app = express();
 
 app.use(express.static("public")); // Make everything inside of public/ available
 
+app.set("views", __dirname + "/views"); // tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
+hbs.registerPartials(__dirname + "/views/partials");//tell HBS wich directory we use for partials 
+
 //app.get(path, code);
 //app.get(path, (req, res, next) => {});
+
+const dataMargarita = {
+  title: "Pizza Margarita",
+  price: 12,
+  recommendedDrink: "beer",
+  imageFile: "pizza-margarita.jpg",
+  ingredients: ["mozzarella", "tomato sauce", "basilicum"],
+};
+
+const dataVeggie = {
+  title: "Veggie Pizza",
+  price: 15,
+  recommendedDrink: "power smoothie",
+  imageFile: "pizza-veggie.jpg",
+  ingredients: ["cherry tomatoes", "basilicum", "Olives"],
+};
+
+const dataSeafood = {
+  title: "Seafood Pizza",
+  recommendedDrink: "white wine",
+  imageFile: "pizza-seafood.jpg",
+  ingredients: ["tomato sauce", "garlic", "prawn"],
+};
 
 //get /
 app.get("/", (request, response, next) => {
@@ -15,12 +44,27 @@ app.get("/", (request, response, next) => {
   //console.log(request.protocol);
   //console.log(request.method);
 
-  response.sendFile(__dirname + "/views/home-page.html");
+  response.render("home-page");
 });
 
 //get /contact
 app.get("/contact", (req, res, next) => {
-    res.sendFile(__dirname + "/views/contact-page.html");
+  res.render("contact-page");
+});
+
+app.get("/pizzas/margarita", (req, res, next) => {
+  //res.send("page for margarita")
+  res.render("product", dataMargarita);
+});
+
+app.get("/pizzas/veggie", (req, res, next) => {
+  //res.send("page for veggie")
+  res.render("product", dataVeggie);
+});
+
+app.get("/pizzas/seafood", (req, res, next) => {
+  //res.send("page for seafood")
+  res.render("product", dataSeafood);
 });
 
 app.listen(3000, () => {
